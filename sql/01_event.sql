7b2b91be3fb2ee972862f2b2d8dbac831a9f92c9
Andreas Schwotzer <andreas.schwotzer@t-online.de>
2015-02-26 19:34:00 +0100

    Update 01_event.sql

--- sql/01_event.sql
+++ sql/01_event.sql
@@ -1,1 +1,12 @@
-use oasdb;
+USE oasdb;
+CREATE TABLE IF NOT EXISTS `events` (
+  `event_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title_short` VARCHAR(20) NULL,
+  `title_long` VARCHAR(128) NULL,
+  `organizer` VARCHAR(45) NULL,
+  `rules` VARCHAR(45) NULL,
+  `currency` VARCHAR(4) NULL COMMENT 'The currency of this event.',
+  `create_time` TIMESTAMP NULL,
+  `update_time` TIMESTAMP NULL,
+  PRIMARY KEY (`event_id`))
+ENGINE = InnoDB
