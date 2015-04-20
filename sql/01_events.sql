d0d696f7ed8cae14de8ac2e168724cb1321ff600
Andreas Schwotzer <andreas.schwotzer@t-online.de>
2015-04-20 18:35:39 +0200

    Update 01_events.sql

--- sql/01_events.sql
+++ sql/01_events.sql
@@ -3,9 +3,11 @@ CREATE TABLE IF NOT EXISTS `events` (
   `title_short` VARCHAR(20) NULL,
   `title_long` VARCHAR(128) NULL,
   `organizer` VARCHAR(45) NULL,
-  `rules` VARCHAR(45) NULL,
   `currency` VARCHAR(4) NULL COMMENT 'The currency of this event.',
   `create_time` TIMESTAMP NULL,
+  `create_user` VARCHAR(20) NULL,
   `update_time` TIMESTAMP NULL,
+  `update_user` VARCHAR(20) NULL,
   PRIMARY KEY (`event_id`))
 ENGINE = InnoDB
+
